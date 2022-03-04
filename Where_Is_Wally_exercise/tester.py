47e164f5f91cc32f691c4363b1df49eee0fe55a1
idabh <idabanghansen@gmail.com>
2022-03-04 14:36:30 +0100

    start of wally app

--- Where_Is_Wally_exercise/tester.py
+++ Where_Is_Wally_exercise/tester.py
@@ -2,42 +2,32 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import glob
 
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
-)
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 2)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-#bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+#---STREAMLIT INTERFACE
+st.markdown("<h1 style='text-align: center; color: red;'>Where is Wally?</h1>", unsafe_allow_html=True)
+st.write('Some explanation text goes here.')
 
+#---SIDEBAR
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
-    
+
+#--ADD SOMETHING HERE TO GET THE IMAGE+GUESS FROM THE MODEL
+
+#---GET IMAGE
+imgs = glob.glob("images/*.jpg") #just the base images right now
+bg_image = imgs[0] #just the first image
 
 # Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    #background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
     height=1500,
     width=1500,
-    drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    drawing_mode='rect',
+    point_display_radius= 0,
     key="canvas",
 )
 
 # Do something interesting with the image data and paths
-if canvas_result.image_data is not None:
-    st.image(canvas_result.image_data)
-if canvas_result.json_data is not None:
-    objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
-    for col in objects.select_dtypes(include=['object']).columns:
-        objects[col] = objects[col].astype("str")
-    st.dataframe(objects)
